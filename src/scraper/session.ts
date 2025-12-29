b3351a22e04a7fd7dce19bf5c0460b1cb3f76a69
Matthew Serrano <mattserrano@me.com>
2025-12-29 08:26:53 -0800

    fix: Exception on Amazon logout

---
+++ src/scraper/session.ts
@@ -0,0 +1,25 @@
+import { BrowserWindow, remote } from 'electron';
+const { BrowserWindow: RemoteBrowserWindow } = remote;
+
+const clearSessionData = (): Promise<boolean> => {
+    const window: BrowserWindow = new RemoteBrowserWindow({
+        width: 1000,
+        height: 600,
+        webPreferences: {
+            webSecurity: false,
+            nodeIntegration: false,
+            partition: 'persist:kindle-highlights',
+        },
+        show: false,
+    });
+
+    return window.webContents.session.clearStorageData()
+        .then(() => {
+            return Promise.resolve(true);
+        }).catch(() => {
+            return Promise.resolve(false);
+        });
+}
+
+
+export default clearSessionData;
